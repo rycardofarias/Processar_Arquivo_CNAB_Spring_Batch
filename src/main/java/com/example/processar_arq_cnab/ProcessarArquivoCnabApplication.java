17fa7cf22254fa3c6a0f5f951db735a3763247cd
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-11-05 16:34:35 -0300

    Start project

---
+++ src/main/java/com/example/processar_arq_cnab/ProcessarArquivoCnabApplication.java
@@ -0,0 +1,13 @@
+package com.example.processar_arq_cnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessarArquivoCnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessarArquivoCnabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/processar_arq_cnab/ProcessarArquivoCnabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.processar_arq_cnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessarArquivoCnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
